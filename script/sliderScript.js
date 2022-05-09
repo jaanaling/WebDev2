8b133c944411b42ff9697cd535d9e4bf2be2c838
Илья Валерьевич Вареник <102265262+jaanaling@users.noreply.github.com>
2022-05-10 02:43:27 +0300

    Add files via upload

---
+++ script/sliderScript.js
@@ -0,0 +1,40 @@
+var links = document.querySelectorAll(".itemLinks");
+var wrapper = document.querySelector("#wrapper");
+
+// activeLink обеспечивает метку для активного элемента
+var activeLink = 0;
+
+// устанавливаем отслеживание событий
+for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    link.addEventListener('click', setClickedItem, false);
+
+    // определяем элемент для activeLink
+    link.itemID = i;
+}
+
+// устанавливаем первый элемент в качестве активного
+links[activeLink].classList.add("active");
+
+function setClickedItem(e) {
+    removeActiveLinks();
+
+    var clickedLink = e.target;
+    activeLink = clickedLink.itemID;
+
+    changePosition(clickedLink);
+}
+
+function removeActiveLinks() {
+    for (var i = 0; i < links.length; i++) {
+        links[i].classList.remove("active");
+    }
+}
+
+// Обработчик изменяет позицию слайдера, после того, как мы убедились,
+// что в качестве активной обозначена нужная нам ссылка.
+function changePosition(link) {
+    link.classList.add("active");
+
+    var position = link.getAttribute("data-pos");
+    wrapper.style.left = position;}
